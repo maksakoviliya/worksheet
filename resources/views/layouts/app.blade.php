758067738eaad0876f6c6ab1cf1aa32dec8bd4a3
maksak_il <maksak_il@mail.ru>
2020-10-30 01:10:20 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
             <div class="flex-1 flex flex-col overflow-hidden">
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 relative">
                     @include('layouts.navbar')
-                    <div class="container mx-auto pr-0 mt-10">
+                    <div class="container mx-auto pr-0 mt-24">
                         @yield('content')
                     </div>
                 </main>
